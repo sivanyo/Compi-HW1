3d26814ba8c6af4cf038880d7de6525154422f86
Mor Paz <1912.mor@gmail.com>
2021-04-09 17:57:45 +0300

    Fix bugs according to tests: Make sure unclosed string error priority is higher than undefined escape sequence Make sure undefined escape prints out the first error encountered and doesn't stack them at print

--- part_a.cpp
+++ part_a.cpp
@@ -61,7 +61,11 @@ void showToken(const int token) {
         exit(0);
     }
     if (token == ERRORSTRING) {
+        bool undefEscape = false;
+        string undef = "";
+        // Strlen returns the length of the string up to the last character -> if the string is hello, strlen is 5
         int strLen = strlen(yytext);
+        //cout << "String is: " << yytext << " and strlen is: " << strLen << endl;
         for (int i = 0; i < strLen; ++i) {
             // Handle newline in the middle of the string, or a '\' as the last character of the string
             if (yytext[i] == '\n' || yytext[i] == '\r' || (yytext[i] == '\\' && i == strLen - 1)) {
@@ -70,23 +74,46 @@ void showToken(const int token) {
             }
             // Handle an illegal escape character sequence
             if (yytext[i] == '\\') {
-                if (yytext[i + 1] != '0' || yytext[i + 1] != '"' || yytext[i + 1] != '\\' || yytext[i + 1] != 't' || yytext[i + 1] != 'x') {
-                    cout << "Error undefined escape sequence " << yytext[i+1] << endl;
-                    exit(0);
+                if (yytext[i + 1] != '0' && yytext[i + 1] != '"' && yytext[i + 1] != '\\' && yytext[i + 1] != 't' && yytext[i + 1] != 'x') {
+                    if (!undefEscape) {
+                        undefEscape = true;
+                        undef.append("Error undefined escape sequence ");
+                        undef.push_back(yytext[i + 1]);
+                    }
+//                    cout << "Error undefined escape sequence " << yytext[i+1] << endl;
+//                    exit(0);
                 }
                 // Check if the embedded hex character value is legal
                 if (yytext[i + 1] == 'x') {
-                    if (i + 3 > strLen) {
+                    if (i + 3 > strLen - 1) {
                         // There is not enough room left in the string for a legal hex value
-                        cout << "Error undefined escape sequence " << yytext[i+1] << yytext[i+2] << endl;
-                        exit(0);
+                        if (!undefEscape) {
+                            undefEscape = true;
+                            undef.append("Error undefined escape sequence ");
+                            undef.push_back(yytext[i + 1]);
+                            undef.push_back(yytext[i + 2]);
+                        }
+//                        cout << "Error undefined escape sequence " << yytext[i+1] << yytext[i+2] << endl;
+//                        exit(0);
                     } else {
                         if (!check_legal_hex_pair(yytext[i + 2], yytext[i + 3])) {
-                            cout << "Error undefined escape sequence " << yytext[i+1] << yytext[i+2] << yytext[i+3] << endl;
+                            if (!undefEscape) {
+                                undefEscape = true;
+                                undef.append("Error undefined escape sequence ");
+                                undef.push_back(yytext[i + 1]);
+                                undef.push_back(yytext[i + 2]);
+                                undef.push_back(yytext[i + 3]);
+                            }
+//                            cout << "Error undefined escape sequence " << yytext[i+1] << yytext[i+2] << yytext[i+3] << endl;
+//                            exit(0);
                         }
                     }
                 }
             }
+        }
+        if (undefEscape) {
+            cout << undef << endl;
+            exit(0);
         }
     }
 
@@ -110,7 +137,7 @@ void showToken(const int token) {
                     // Need to replace the escape sequence with it's real escape sequence meaning
                     int result = replace_escape_sequence(yytext[i + 1]);
                     if (result == -1) {
-                        cout << "Error undefined escape sequence " << yytext[i+1] << endl;
+                        cout << "Error undefined escape sequence " << yytext[i + 1] << endl;
                         exit(0);
                     } else {
                         output.push_back(result);
